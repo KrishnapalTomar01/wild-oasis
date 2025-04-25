4ac13dea84324d055169e4599c6a40160f67ca0e
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2025-04-26 00:21:17 +0530

    Fixed env api key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://sshfuukiwewewnmmtssv.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzaGZ1dWtpd2V3ZXdubW10c3N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2MDEzMjQsImV4cCI6MjA2MTE3NzMyNH0.pMWeYxMNr0mLrrk7zbMDFzNwSbiZXKhjfVj-GztmIZg";
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
